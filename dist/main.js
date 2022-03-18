27ff602a54bacdebe00377a6fd50cfd3045423b4
caoxinke@supermap.com <caoxinke@supermap.com>
2022-03-18 20:51:00 +0800

    fix: 新增测试

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("console.log('Hello World!');\n\n//# sourceURL=webpack://my-webpack-project/./src/index.js?");
+eval("console.log(\"Hello World!\");\n\n//# sourceURL=webpack://my-webpack-project/./src/index.js?");
 
 /***/ })
 
