3b4fbf63c2d5eb36d245e11c6f91f06a1709998e
caoxinke@supermap.com <caoxinke@supermap.com>
2022-03-18 11:29:45 +0800

    第一次提交

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    root: true,
+	env: {
+		node: true,
+		browser: true,
+		es6: true,
+	},
+    extends: "eslint:recommended",
+    rules: {
+    // enable additional rules
+        indent: ["error", 4,],
+        "linebreak-style": ["error", "unix",],
+        quotes: ["error", "double",],
+        semi: ["error", "always",],
+
+        // override default options for rules from base configurations
+        "comma-dangle": ["error", "always",],
+        "no-cond-assign": ["error", "always",],
+
+        // disable rules from base configurations
+        "no-console": "off",
+    },
+};
