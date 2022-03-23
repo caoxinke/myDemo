9b99ef3fffb4bf483a437e8f38cd62a4e2a8bb2e
xinkecao@163.com <xinkecao@163.com>
2022-03-23 13:18:14 +0800

    fix: js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,26 @@
 module.exports = {
-    root: true,
-	env: {
-		node: true,
-		browser: true,
-		es6: true,
-	},
-    extends: "eslint:recommended",
-    rules: {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  extends: "eslint:recommended",
+  rules: {
     // enable additional rules
-        indent: ["error", 4,],
-        "linebreak-style": ["error", "unix",],
-        quotes: ["error", "double",],
-        semi: ["error", "always",],
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
 
-        // override default options for rules from base configurations
-        "comma-dangle": ["error", "always",],
-        "no-cond-assign": ["error", "always",],
+    // override default options for rules from base configurations
+    "comma-dangle": ["error", "always"],
+    "no-cond-assign": ["error", "always"],
 
-        // disable rules from base configurations
-        "no-console": "off",
-    },
+    // disable rules from base configurations
+    "no-console": "off",
+  },
+  parserOptions: {
+    sourceType: "module",
+  },
 };
